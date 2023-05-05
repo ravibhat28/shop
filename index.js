f7c0f8119816895638c1814c65a562d719c5ee4b
ravibhat28 <121099493+ravibhat28@users.noreply.github.com>
2023-05-05 12:21:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav1() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav1() {
+    document.getElementById("mySidenav").style.width = "0";
+}
